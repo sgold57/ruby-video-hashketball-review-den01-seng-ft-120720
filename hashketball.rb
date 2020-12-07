25fc379d84780563889858278fa88c3b00bda914
Sam Gold <sgold5757@gmail.com>
2020-12-07 03:23:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -152,9 +152,7 @@ end
 
 def team_colors(team)
   game_hash.each do |squad, specs|
-    binding.pry
     if specs[:team_name] == team
-      binding.pry
       return specs[:colors]
     end
   end
